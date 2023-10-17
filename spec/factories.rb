3278b2afde6603a74959ecb052fe02ebe35b711c
cuilu fan <cuilu.fan@gmail.com>
2023-10-17 22:08:20 +0200

    factories.rb fixed with less blocks

--- spec/factories.rb
+++ spec/factories.rb
@@ -7,30 +7,27 @@ FactoryBot.define do
     email { Faker::Internet.unique.email }
     password { "123456" }
   end
-
   factory :call do
     duration { 20 }
   end
-
   factory :message do
     text { Faker::Lorem.sentence }
   end
+end
 
+FactoryBot.define do
   factory :translation do
     primary_language_word { "Apfel" }
     target_language_word { "Apple" }
     default { false }
   end
-
   factory :review do
     rating { Faker::Number.between(from: 1, to: 5) }
   end
-
   factory :category do
     name { "Fruits" }
     default { false }
   end
-
   factory :user_flashcard do
     success { true }
     failed { false }
