920d92c4b11edf74f4e5ef6256a45062cf1ee9be
shakar hassan <shakarmohammed2002@gmail.com>
2023-10-17 13:05:26 +0200

    user and call model test is done

--- db/migrate/20231017083438_create_calls.rb
+++ db/migrate/20231017083438_create_calls.rb
@@ -2,7 +2,8 @@ class CreateCalls < ActiveRecord::Migration[7.0]
   def change
     create_table :calls do |t|
       t.integer :duration
-      t.references :user, null: false, foreign_key: true
+      t.references :caller, null: false, foreign_key: { to_table: :users }
+      t.references :random_user, null: false, foreign_key: { to_table: :users }
 
       t.timestamps
     end
--- spec/models/call_spec.rb
+++ spec/models/call_spec.rb
@@ -4,4 +4,11 @@ RSpec.describe Call, type: :model do
   context 'with DB columns' do
     it { is_expected.to have_db_column(:duration).of_type :integer }
   end
+  context "associations" do
+    it { is_expected.to belong_to(:caller).class_name("User") }
+    it { is_expected.to belong_to(:random_user).class_name("User") }
+  end
+  context "validations" do
+    it { is_expected.to validate_presence_of(:duration) }
+  end
 end
