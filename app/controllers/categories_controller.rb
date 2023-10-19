8b0896271d90451d362897c2ac041d95063516a6
cuilu fan <cuilu.fan@gmail.com>
2023-10-19 20:51:48 +0200

    controller and view files set up done

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,14 @@
+class CategoriesController < ApplicationController
+  # Max, can you please work on the index method?
+  # So that all categories will be shown on the path /categories
+  def index
+  end
+
+  # Cuilu will work on the create and show method
+  # So that a user can create a category for translated words
+  def create
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
-resources :user_flashcards, only: [:new, :create, :destroy]
+  resources :user_flashcards, only: [:new, :create, :destroy]
+  resources :categories, only: %i[index new create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
