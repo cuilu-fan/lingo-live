da9867745cc53813eadcc13d4fdcb9aabf63a639
cuilu fan <cuilu.fan@gmail.com>
2023-10-20 18:03:03 +0200

    added category to user_flashcard

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -7,20 +7,7 @@ class CategoriesController < ApplicationController
 
   # Cuilu will work on the create and show method
   # So that a user can create a category for translated words
-  def create
-    @category = Category.new(category_params)
-    @category = @user_flashcard.category_id
-    @category.default = false if @category.save
-  end
-
   def show
     @category = Category.find(params[:id])
   end
-
-  private
-
-  def category_params
-    params.require(:category).permit(:id, :name)
-  end
-
 end
--- app/controllers/user_flashcards_controller.rb
+++ app/controllers/user_flashcards_controller.rb
@@ -11,13 +11,19 @@ class UserFlashcardsController < ApplicationController
     @user_flashcard.save
   end
 
-  def create_flashcard
-    
+  def create_category
+    @category = current_user.category_id
+    @category_new = Category.new
+    @category_new.save if @category.default == false
   end
 
   private
 
   def user_flashcard_params
     params.require(:user_flashcard).permit(:flashcard_id, :category_id)
+  end
+
+  def category_params
+    params.require(:category).permit(:category_id, :name, :default)
   end
 end
