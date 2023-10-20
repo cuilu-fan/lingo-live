39632dabef8dae527ecfee46d8853d91ac69010d
Max223871 <julianmaxin007@gmail.com>
2023-10-20 14:58:06 +0200

    added index for categories backend and front end

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,13 +2,15 @@ class CategoriesController < ApplicationController
   # Max, can you please work on the index method?
   # So that all categories will be shown on the path /categories
   def index
+    @categories = Category.all
   end
 
   # Cuilu will work on the create and show method
   # So that a user can create a category for translated words
-  def create
-  end
 
   def show
+  end
+
+  def create
   end
 end
