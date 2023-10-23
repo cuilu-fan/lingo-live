d50975cb30448dafad583f6413ae48ec9c28f6f1
cuilu fan <cuilu.fan@gmail.com>
2023-10-23 13:07:20 +0200

    added filter to category through user_flashcards

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,12 +2,13 @@ class CategoriesController < ApplicationController
   # Max, can you please work on the index method?
   # So that all categories will be shown on the path /categories
   def index
-    @categories = Category.all
+    @categories = Category.joins(:user_flashcards).uniq
   end
 
   # Cuilu will work on the create and show method
   # So that a user can create a category for translated words
   def show
     @category = Category.find(params[:id])
+    @flashcards = UserFlashcard.where(user: current_user).joins(:translation)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_17_195323) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_23_100259) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -69,8 +69,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_17_195323) do
   create_table "user_flashcards", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "category_id", null: false
-    t.boolean "success"
-    t.boolean "failed"
+    t.integer "success", default: 0
+    t.integer "failed", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "translation_id", null: false
