54e92d10381e663e5cc254eeecea820f79201fe4
cuilu fan <cuilu.fan@gmail.com>
2023-10-23 19:09:17 +0200

    changed table column name in user_flashcards

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_23_100259) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_23_170717) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -69,8 +69,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_23_100259) do
   create_table "user_flashcards", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "category_id", null: false
-    t.integer "success", default: 0
-    t.integer "failed", default: 0
+    t.integer "success_count", default: 0
+    t.integer "failed_count", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "translation_id", null: false
