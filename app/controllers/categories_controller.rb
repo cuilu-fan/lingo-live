634237c17ab6b3d2234779d4077392f26509290f
cuilu fan <cuilu.fan@gmail.com>
2023-10-27 17:36:17 +0200

    review logic completed

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -10,17 +10,21 @@ class CategoriesController < ApplicationController
   # So that a user can create a category for translated words
   def show
     @category = Category.find(params[:id])
-    flashcards = UserFlashcard.where(user: current_user, category: @category, known: false)
-                              .joins(:translation)
-                              .order(:success_count, :failed_count)
+    flashcards = UserFlashcard.flashcards_to_review(current_user, @category)
     if flashcards.empty?
-      known_flashcards = UserFlashcard.where(user: current_user, category: @category, known: true)
-      known_flashcards.map do |known_flashcard|
-        known_flashcard.known = false
-        known_flashcard.save
-      end
+      reset_review_deck
       redirect_to categories_path, notice: SUCCESS_MESSAGE
     end
     @flashcard = flashcards.first
+  end
+
+  private
+
+  def reset_review_deck
+    UserFlashcard.where(user: current_user, category: @category, known: true)
+                 .map do |known_flashcard|
+      known_flashcard.known = false
+      known_flashcard.save!
+    end
   end
 end
