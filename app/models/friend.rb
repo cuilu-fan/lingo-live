c17f165134181fb6dc7d5dd9fc93ef1f530855bf
James Hibbeard <jhibbeard@gmail.com>
2023-10-30 12:04:00 +0100

    added chatroom model, and updated message

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,5 +1,5 @@
 class Friend < ApplicationRecord
   belongs_to :user_1, foreign_key: 'user_1_id', class_name: 'User'
   belongs_to :user_2, foreign_key: 'user_2_id', class_name: 'User'
-  has_many :messages
+  has_many :chatrooms
 end
