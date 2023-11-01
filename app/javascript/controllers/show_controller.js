38ea5749b608bf8fba73ce923a5df2f22297c7d2
Cuilu Fan <cuilu.fan@gmail.com>
2023-11-01 16:31:50 +0100

    changed navbar height

--- app/javascript/controllers/show_controller.js
+++ app/javascript/controllers/show_controller.js
@@ -1,20 +1,26 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="show"
 export default class extends Controller {
-  static targets = ["showbuttonElement", "frontElement", "backElement", "reviewElement", "knownElement"]
+  static targets = [
+    "showbuttonElement",
+    "frontElement",
+    "backElement",
+    "reviewElement",
+    "knownElement",
+  ];
   connect() {
-    console.log("hello")
-    console.log(this.frontElementTarget)
+    console.log("hello");
+    console.log(this.frontElementTarget);
   }
 
   reveal() {
     // Here: "show" button gone, two review options button shown
-    this.backElementTarget.classList.toggle("d-none")
-    this.showbuttonElementTarget.classList.toggle("d-none")
-    this.reviewElementTarget.classList.toggle("d-none")
-    this.knownElementTarget.classList.toggle("d-none")
+    this.frontElementTarget.classList.toggle("d-none");
+    this.backElementTarget.classList.toggle("d-none");
+    this.showbuttonElementTarget.classList.toggle("d-none");
+    this.reviewElementTarget.classList.toggle("d-none");
+    this.knownElementTarget.classList.toggle("d-none");
     // Here: two review options button gone, "show" button shown again
-
   }
 }
