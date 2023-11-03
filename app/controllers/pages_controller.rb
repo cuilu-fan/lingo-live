535a70a8aa9ec1c7c81d394713510be5c7d77094
Cuilu Fan <cuilu.fan@gmail.com>
2023-11-03 16:27:39 +0100

    homepage in progress

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   # skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    @translations = Translation.last(4)
+    @translations = Translation.last(2)
   end
 end
