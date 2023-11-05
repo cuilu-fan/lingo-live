83cfcb5d5651546b9fed5449b93def5f751e2a64
Juan Laborde <juan.laborde33@gmail.com>
2023-11-05 15:30:10 +0100

    Video connection works

--- app/models/call.rb
+++ app/models/call.rb
@@ -5,6 +5,6 @@ class Call < ApplicationRecord
   before_create do
     opentok = OpenTok::OpenTok.new ENV.fetch('VONAGE_API_KEY', nil), ENV.fetch('VONAGE_API_SECRET', nil)
     session = opentok.create_session
-    self.vonage_session_id = session.session_id
+    self.session_id = session.session_id
   end
 end
