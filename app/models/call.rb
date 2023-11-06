636d90ba5271f2a03d7fad05404ccd9665350bcb
shakar hassan <shakarmohammed2002@gmail.com>
2023-11-06 10:44:16 +0100

    video is in progress

--- app/models/call.rb
+++ app/models/call.rb
@@ -3,7 +3,7 @@ class Call < ApplicationRecord
   belongs_to :random_user, foreign_key: 'random_user_id', class_name: 'User'
   validates :duration, presence: true, numericality: { only_integer: true }
   before_create do
-    opentok = OpenTok::OpenTok.new ENV.fetch('VONAGE_API_KEY', nil), ENV.fetch('VONAGE_API_SECRET', nil)
+    opentok = OpenTok::OpenTok.new "47801941", "55cbe0f76611e46f7c79d86f2b23b2c60296fbea"
     session = opentok.create_session
     self.session_id = session.session_id
   end
