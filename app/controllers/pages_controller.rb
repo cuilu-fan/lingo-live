0a11e3a0c41e63e61e130a47761fc2b451ace676
Cuilu Fan <cuilu.fan@gmail.com>
2023-11-09 10:12:24 +0100

    changed homepage flashcard view to current user

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   # skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    @translations = Translation.last(2)
+    @translations = current_user.translations.last(2)
   end
 end
